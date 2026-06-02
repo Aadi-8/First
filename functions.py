54b487bc8389915650ce8b599e8c75c5827d6250
Aadi8 <raiaman0705@gmail.com>
2026-06-02 15:13:53 +0530

    funtions with loop

--- functions.py
+++ functions.py
@@ -28,8 +28,25 @@ fun1(8,13)
 def fun2(a,b):
         pass
 
+#fixed argument and return calling without print 
+def fun3(a=3,b=5):
+        return float((a+b)/2)  #return is a keyword not a veriable do not use = sign 
+    
+c=fun3(6,8)     #fun replace the value it is not bind to default value
+d=fun3()        #for this the function use the default values
+e=fun3(4)       #here a=4 and b i default 5
+print(c)       
+print(d)        
+print(e)      
 
-        
-          
+def fun4(*numbers):
+        sum=0
+        for i in numbers:
+                sum = (sum + i)
+        # t = (sum)/len(numbers)      # this should be indented in def 
+        # print(t)                  we can use this but we are using return
+        return sum/len(numbers)
+f=fun4(2,3,7,8)
+print(f)
 
 
